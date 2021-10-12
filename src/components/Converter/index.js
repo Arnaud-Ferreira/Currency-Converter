93caf25ab133429bbf2bcc042eceec224c43e1a1
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2021-10-12 13:19:19 +0200

    Dynamisation

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-import './style.scss';
-
 import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
 import Amount from 'src/components/Amount';
 
+// Data import
+import currenciesData from 'src/data/currencies';
+
+import './style.scss';
+
 export default function Converter() {
   return (
     <div className="converter">
-      <Header />
-      <Currencies />
-      <Amount />
+      <Header baseAmount={14} />
+      <Currencies currencies={currenciesData}/>
+      <Amount value={1.09} currency="United States Dollar" />
     </div>
   );
 }
--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,29 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Currency from './Currency';
+
 import './style.scss';
 
-export default function Currencies() {
+export default function Currencies({ currencies }) {
+
+  // const currencyList = currencies.map((currency) => {
+  //   return <Currency key={currency.name} {...currency} />;
+  // });
+  const currencyList = currencies.map((currency) => <Currency key={currency.name} {...currency} />);
+  
   return (
     <div className="currencies">
       <p className="currencies__title">Currencies</p>
       <ul className="currencies__list">
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
-        <Currency />
+        {currencyList}
       </ul>
     </div>
   );
 }
+
+Currencies.propTypes = {
+
+// We know that the array contains objects and we know the shape of these objects
+  currencies: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    rate: PropTypes.number.isRequired,
+  })).isRequired,
+};
